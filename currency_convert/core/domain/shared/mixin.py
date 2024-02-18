0b13935012b4fab82b56eecd3aa95d92c954f165
Markus Iorio <mbalzert@gmail.com>
2024-02-18 21:06:51 +0100

    created timestamp and id mixin

--- currency_convert/core/domain/shared/mixin.py
+++ currency_convert/core/domain/shared/mixin.py
@@ -6,6 +6,7 @@ import functools
 import pydantic
 
 from currency_convert.core.domain.resources import strings_error
+from currency_convert.core.domain.shared.value_objects.uuidid import UUIDID
 
 UTC_NOW = functools.partial(datetime.datetime.now, datetime.timezone.utc)
 
@@ -25,3 +26,11 @@ class DateTimeMixin(pydantic.BaseModel):
             case _:
                 raise ValueError(strings_error.INVALID_VALUE % value)
         return value
+
+
+class TimestampMixin(pydantic.BaseModel):
+    timestamp: datetime.datetime = pydantic.Field(default_factory=UTC_NOW)
+
+
+class IDMixin(pydantic.BaseModel):
+    id_: UUIDID = pydantic.Field(default_factory=UUIDID.create)
