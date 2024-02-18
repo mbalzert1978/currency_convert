502e271dced12de976caf065d5099dd988bec1d9
Markus Iorio <mbalzert@gmail.com>
2024-02-18 15:53:54 +0100

    entity base class

---
+++ currency_convert/core/domain/shared/entity.py
@@ -0,0 +1,25 @@
+import abc
+import typing
+
+import pydantic
+
+from currency_convert.core.domain.shared.domain_event import DomainEvent
+from currency_convert.core.domain.shared.uuid_valueobject import UUIDID
+
+TV = typing.TypeVar("TV")
+
+
+class Entity(pydantic.BaseModel, abc.ABC):
+    id_: UUIDID = pydantic.Field(default_factory=UUIDID)
+    domain_events: list[DomainEvent] = pydantic.Field(
+        default_factory=list,
+        init=False,
+    )
+
+    def __eq__(self, __value: object) -> bool:
+        if isinstance(__value, type(self)):
+            return self.id_.value == __value.id_.value
+        return False
+
+    def send(self, domain_event: DomainEvent) -> None:
+        self.domain_events.append(domain_event)
