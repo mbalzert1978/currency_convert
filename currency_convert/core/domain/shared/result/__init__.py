ab0380cedd95a3bbedb8ffca0ad7c9b792487180
Markus Iorio <mbalzert@gmail.com>
2024-02-21 18:57:58 +0100

    init

---
+++ currency_convert/core/domain/shared/result/__init__.py
@@ -0,0 +1,8 @@
+from currency_convert.core.domain.shared.result.result import (
+    Failure,
+    Result,
+    Success,
+    UnwrapFailedError,
+)
+
+__all__ = ["Result", "Success", "Failure", "UnwrapFailedError"]
