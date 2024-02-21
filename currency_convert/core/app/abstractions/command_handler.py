b20a371f637aca825f157d9cc7b0b4bc1f9d711d
Markus Iorio <mbalzert@gmail.com>
2024-02-21 22:13:00 +0100

    command handler interface

---
+++ currency_convert/core/app/abstractions/command_handler.py
@@ -0,0 +1,12 @@
+import typing
+
+from currency_convert.core.domain.shared.command import Command
+from currency_convert.core.domain.shared.result.result import Result
+
+_TC_contra = typing.TypeVar("_TC_contra", bound=Command, contravariant=True)
+_RV_co = typing.TypeVar("_RV_co", bound=Result, covariant=True)
+
+
+class CommandHandler(typing.Protocol[_TC_contra, _RV_co]):
+    def handle(self, cmd: _TC_contra) -> _RV_co:
+        ...
