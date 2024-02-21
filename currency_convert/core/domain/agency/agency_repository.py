8a6db78d5e1003b4c92e0637695fc4f9233a57f8
Markus Iorio <mbalzert@gmail.com>
2024-02-21 22:13:21 +0100

    agency repository

---
+++ currency_convert/core/domain/agency/agency_repository.py
@@ -0,0 +1,27 @@
+import types
+import typing
+
+from currency_convert.core.domain.agency.entity import Agency
+from currency_convert.core.domain.shared.error import Error
+from currency_convert.core.domain.shared.result.result import Result
+
+
+class IAgencyRepository(typing.Protocol):
+    def __enter__(self) -> typing.Self:
+        ...
+
+    def __exit__(
+        self,
+        __exc_type: typing.Optional[type[BaseException]],
+        __exc_value: typing.Optional[BaseException],
+        __traceback: typing.Optional[types.TracebackType],
+    ) -> Result[typing.Optional[bool], Error]:
+        ...
+
+    def find_by_name(
+        self, name: str
+    ) -> Result[typing.Optional[Agency], Error]:
+        ...
+
+    def add(self, agency: Agency) -> Result[None, Error]:
+        ...
