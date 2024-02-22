c602da53eb4e7efcc21317e72e93d0379450bdd9
Iorio Markus <markus.iorio@bfd.de>
2024-02-22 16:17:01 +0100

    agency not found err

--- currency_convert/core/domain/agency/errors.py
+++ currency_convert/core/domain/agency/errors.py
@@ -3,3 +3,7 @@ from currency_convert.core.domain.shared.error import Error
 
 class AgencyAllreadExistsError(Error):
     """Error when trying to create an agency that already exists."""
+
+
+class AgencyNotFoundError(Error):
+    """Error when there is no agency with the given name in the database."""
