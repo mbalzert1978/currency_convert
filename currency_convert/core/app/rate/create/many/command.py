0c5b107bff5a6b1b6739467978b5118059c4ccc4
Iorio Markus <markus.iorio@bfd.de>
2024-02-22 16:16:12 +0100

    create rates usecase

---
+++ currency_convert/core/app/rate/create/many/command.py
@@ -0,0 +1,9 @@
+from typing import Sequence
+
+from currency_convert.core.domain.rate.entity import Rate
+from currency_convert.core.domain.shared.command import Command
+
+
+class CreateRates(Command):
+    agency_name: str
+    rates: Sequence[Rate]
