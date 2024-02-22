3aff216093865e03191bb951d999df8d5b4b60d5
Iorio Markus <markus.iorio@bfd.de>
2024-02-22 16:16:48 +0100

    separeted mixins

--- currency_convert/core/domain/shared/mixin.py
+++ currency_convert/core/domain/shared/mixin.py
@@ -2,8 +2,10 @@ from __future__ import annotations
 
 import datetime
 import functools
+import typing
 
 import pydantic
+import pydantic.functional_validators as pfv
 
 from currency_convert.core.domain.resources import strings_error
 from currency_convert.core.domain.shared.value_objects.uuidid import UUIDID
@@ -11,21 +13,23 @@ from currency_convert.core.domain.shared.value_objects.uuidid import UUIDID
 UTC_NOW = functools.partial(datetime.datetime.now, datetime.timezone.utc)
 
 
-class DateTimeMixin(pydantic.BaseModel):
-    created_at: datetime.datetime | None = None
-    updated_at: datetime.datetime | None = None
+def _set_datetime_with_timezone(value: datetime.datetime | None) -> datetime.datetime:
+    match value:
+        case None:
+            value = UTC_NOW()
+        case datetime.datetime() if value.tzinfo is None:
+            value = value.replace(tzinfo=datetime.timezone.utc)
+        case _:
+            raise ValueError(strings_error.INVALID_VALUE % value)
+    return value
 
-    @pydantic.field_validator("created_at", "updated_at", mode="before")
-    @classmethod
-    def validate_(cls, value: datetime.datetime | None) -> datetime.datetime:
-        match value:
-            case None:
-                value = UTC_NOW()
-            case datetime.datetime() if value.tzinfo is None:
-                value = value.replace(tzinfo=datetime.timezone.utc)
-            case _:
-                raise ValueError(strings_error.INVALID_VALUE % value)
-        return value
+
+class CreatedAtMixin(pydantic.BaseModel):
+    created_at: typing.Annotated[datetime.datetime | None, pfv.BeforeValidator(_set_datetime_with_timezone)]
+
+
+class UpdatedAtMixin(pydantic.BaseModel):
+    updated_at: typing.Annotated[datetime.datetime | None, pfv.BeforeValidator(_set_datetime_with_timezone)]
 
 
 class TimestampMixin(pydantic.BaseModel):
