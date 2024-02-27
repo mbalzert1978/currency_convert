b4e55c3e1d1b616e0a92bae1fdb907dedf1db162
Iorio Markus <markus.iorio@bfd.de>
2024-02-27 12:18:41 +0100

    typo

--- currency_convert/core/app/rate/create/many/handler.py
+++ currency_convert/core/app/rate/create/many/handler.py
@@ -13,12 +13,12 @@ NOT_FOUND_MSG = "No agency with name: %s found."
 class CreateRatesHandler(CommandHandler[CreateRates, Result[None, Error]]):
     def __init__(self, agency_repository: IAgencyRepository, rate_repository: IRateRepository) -> None:
         self._agency_repository = agency_repository
-        self._rate_repoitory = rate_repository
+        self._rate_repository = rate_repository
 
     def handle(self, cmd: CreateRates) -> Result[None, Error]:
         if self._agency_repository.find_by_name(cmd.agency_name).is_failure():
             return Result.from_failure(AgencyNotFoundError(404, NOT_FOUND_MSG % cmd.agency_name))
-        with self._rate_repoitory as repo:
+        with self._rate_repository as repo:
             db_result = repo.add_many(cmd.rates)
         if db_result.is_failure():
             return Result.from_failure(db_result)
