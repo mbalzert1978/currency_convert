e2a449d14c3e06455a5ef1805e7365a8d42ac0e8
Iorio Markus <markus.iorio@bfd.de>
2024-02-27 12:19:17 +0100

    remove creation of the agency out of the Create Rate Handler

--- currency_convert/core/app/rate/create/single/command.py
+++ currency_convert/core/app/rate/create/single/command.py
@@ -1,12 +1,9 @@
 from currency_convert.core.domain.shared.command import Command
-from currency_convert.core.domain.shared.value_objects.country import Country
 from currency_convert.core.domain.shared.value_objects.currency_code import CurrencyCode
 from currency_convert.core.domain.shared.value_objects.money import Money
 
 
 class CreateRate(Command):
     agency_name: str
-    base_currency: CurrencyCode
     to_currency: CurrencyCode
     rate: Money
-    residing_coutry: Country
--- currency_convert/core/app/rate/create/single/handler.py
+++ currency_convert/core/app/rate/create/single/handler.py
@@ -1,41 +1,25 @@
 from currency_convert.core.app.abstractions.command_handler import CommandHandler
 from currency_convert.core.app.rate.create.single.command import CreateRate
 from currency_convert.core.domain.agency.agency_repository import IAgencyRepository
-from currency_convert.core.domain.agency.entity import Agency
 from currency_convert.core.domain.rate.entity import Rate
 from currency_convert.core.domain.rate.rate_repository import IRateRepository
 from currency_convert.core.domain.shared.error import Error
 from currency_convert.core.domain.shared.result.result import Result
-from currency_convert.core.domain.shared.value_objects.uuidid import UUIDID
 
 
 class CreateRateHandler(CommandHandler[CreateRate, Result[None, Error]]):
     def __init__(self, agency_repository: IAgencyRepository, rate_repository: IRateRepository) -> None:
         self._agency_repository = agency_repository
-        self._rate_repoitory = rate_repository
+        self._rate_repository = rate_repository
 
     def handle(self, cmd: CreateRate) -> Result[None, Error]:
         with self._agency_repository as repo:
             get_result = repo.find_by_name(cmd.agency_name)
 
-        if (get_result).is_success() and (agency := get_result.unwrap()).is_some():
-            return self._insert_rate(agency.unwrap().id_, cmd)
+        if get_result.is_failure() or (agency := get_result.unwrap()).is_none():
+            return Result.from_failure(get_result.failure())
 
-        in_agency = Agency.create(
-            name=cmd.agency_name,
-            base_currency=cmd.base_currency,
-            residing_country=cmd.residing_coutry,
-        )
-
-        with self._agency_repository as repo:
-            in_result = repo.add(in_agency)
-
-        if (in_result).is_success():
-            return self._insert_rate(in_agency.id_, cmd)
-        return in_result
-
-    def _insert_rate(self, agency_id: UUIDID, cmd: CreateRate) -> Result[None, Error]:
-        in_rate = Rate.create(agency_id, cmd.to_currency, cmd.rate)
-        with self._rate_repoitory as repo:
+        in_rate = Rate.create(agency.unwrap().id_, cmd.to_currency, cmd.rate)
+        with self._rate_repository as repo:
             write_result = repo.add(in_rate)
         return Result.from_value(None) if (write_result).is_success() else write_result
