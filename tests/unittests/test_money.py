e7765d945aa0394bf73d7fc124a83a2489f5605f
Markus Iorio <mbalzert@gmail.com>
2024-05-27 21:23:39 +0200

    neg result

--- tests/unittests/test_money.py
+++ tests/unittests/test_money.py
@@ -1,6 +1,7 @@
 import decimal
 
 from currency_convert.domain.agency.valueobjects.money import Money
+from currency_convert.domain.primitives.error import GenericError
 
 
 def test_create_ok() -> None:
@@ -8,3 +9,10 @@ def test_create_ok() -> None:
 
     assert result.is_ok()
     assert result.unwrap() == Money(decimal.Decimal("100.00000000"))
+
+
+def test_create_err_negative_value() -> None:
+    result = Money.create(-100)
+
+    assert result.is_err()
+    assert isinstance(result.unwrap_err(), GenericError)
