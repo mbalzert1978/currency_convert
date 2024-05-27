30cd73f788bc1fb65a640fe965d0ea78640617a7
Markus Iorio <mbalzert@gmail.com>
2024-05-27 21:16:35 +0200

    money

--- currency_convert/domain/agency/valueobjects/money.py
+++ currency_convert/domain/agency/valueobjects/money.py
@@ -5,25 +5,29 @@ import decimal
 import typing
 from typing import Iterator
 
+from result import Result
+
 from currency_convert.domain.primitives.error import GenericError
 from currency_convert.domain.primitives.valueobject import ValueObject
 
 _Decimal: typing.TypeAlias = (
     decimal.Decimal | float | str | tuple[int, typing.Sequence[int], int]
 )
+_PRECISION = decimal.Decimal(10) ** -8
 
 
 @dataclasses.dataclass(frozen=True, slots=True, eq=False)
 class Money(ValueObject[decimal.Decimal]):
-    _PRECISION = dataclasses.field(default=decimal.Decimal(10) ** -8, init=False)
     value: decimal.Decimal
 
     def get_atomic_values(self) -> Iterator[decimal.Decimal]:
         yield self.value
 
     @classmethod
-    def create(cls, value: _Decimal) -> Money:
+    def create(cls, value: _Decimal) -> Result[Money, GenericError]:
         try:
-            return cls(decimal.Decimal(value).quantize(cls._PRECISION).copy_abs())
+            r = cls(decimal.Decimal(value).quantize(_PRECISION).copy_abs())
         except decimal.InvalidOperation as exc:
-            raise GenericError from exc
+            return Result.Err(GenericError.from_exc(exc))
+        else:
+            return Result.Ok(r)
