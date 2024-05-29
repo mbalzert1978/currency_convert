1a4d91855b15317b092f57ec42828ead8ab668d6
Markus Iorio <mbalzert@gmail.com>
2024-05-29 22:44:16 +0200

    fn tests functional

--- currency_convert/domain/agency/entities/agency.py
+++ currency_convert/domain/agency/entities/agency.py
@@ -2,18 +2,19 @@ from __future__ import annotations
 
 import dataclasses
 import logging
-from datetime import datetime, timedelta
-from typing import Sequence
+import typing
+from typing import Never, Sequence
 
-from result import Result
+from option import Option, Some
+from result import Err, Result
 from typing_extensions import TypedDict
 
-from currency_convert.domain.agency.entities.interface import RatesRepository
 from currency_convert.domain.agency.valueobjects.currency import Currency
 from currency_convert.domain.agency.valueobjects.money import _Decimal
 from currency_convert.domain.agency.valueobjects.rate import Rate
 from currency_convert.domain.primitives.entity import AggregateRoot
-from currency_convert.domain.primitives.error import GenericError
+
+from ...primitives.error import GenericError
 
 _logger = logging.getLogger(__name__)
 
@@ -29,119 +30,77 @@ class UnprocessableRate(UnprocessedRate):
     pass
 
 
+TIME_PERIOD = 5
+
+
 @dataclasses.dataclass(slots=True, eq=False)
 class Agency(AggregateRoot):
     base: Currency
     name: str
     country: str
-    rates_repository: RatesRepository
+    rates: list[Rate] = dataclasses.field(default_factory=list)
 
     def add_rate(
-        self,
-        currency_from: str,
-        currency_to: str,
-        rate: _Decimal,
-        date: str,
-    ) -> Result[None, GenericError]:
-        if (r := Rate.create(currency_from, currency_to, rate, date)).is_err():
-            return Result.Err(GenericError())
-        return self.rates_repository.add(r.unwrap())
+        self, currency_from: str, currency_to: str, rate: _Decimal, date: str
+    ) -> Result[None, Never]:
+        return Rate.create(currency_from, currency_to, rate, date).and_then(
+            Result.as_result(self.rates.append)
+        )
 
     def add_rates(
         self, rates: list[UnprocessedRate]
     ) -> Result[None, list[UnprocessableRate]]:
-        unprocessable_rates = []
-        for rate in rates:
-            try:
-                if (
-                    r := Rate.create(
-                        rate["currency_from"],
-                        rate["currency_to"],
-                        rate["rate"],
-                        rate["date"],
-                    )
-                ).is_err():
-                    _logger.error(f"Invalid rate: {rate}")
-                    unprocessable_rates.append(rate)
-                    continue
-                self.rates_repository.add(r.unwrap())
-            except KeyError:
-                unprocessable_rates.append(rate)
-        if unprocessable_rates:
-            return Result.Err(unprocessable_rates)
-        return Result.Ok(None)
-
-    def _get_rate_for_date_range(
-        self, currency_from: str, currency_to: str, date: str
-    ) -> Result[Rate, GenericError]:
-        if (rates := self.rates_repository.get_all()).is_err():
-            return Result.Err(GenericError())
-
-        target_date = datetime.strptime(date, "%Y-%m-%d")
-        for days_back in range(6):
-            check_date = (target_date - timedelta(days=days_back)).strftime("%Y-%m-%d")
-            rate: Result[Rate, GenericError] | None = next(
-                (
-                    Result.Ok(rate)
-                    for rate in rates.unwrap()
-                    if rate.currency_from.code == currency_from
-                    and rate.currency_to.code == currency_to
-                    and rate.date == check_date
-                ),
-                None,
+        unprocessable = [
+            rate
+            for rate in rates
+            if Rate.create(
+                rate["currency_from"], rate["currency_to"], rate["rate"], rate["date"]
             )
-            if rate:
-                return rate
-
-        return Result.Err(GenericError())
-
-    def _get_direct_rate(
-        self, currency_from: str, currency_to: str, date: str
-    ) -> Result[Rate, GenericError]:
-        return self._get_rate_for_date_range(currency_from, currency_to, date)
-
-    def _find_base_rates(
-        self, currency_from: str, currency_to: str, date: str
-    ) -> tuple[Rate | None, Rate | None]:
-        rate_from_base = self._get_rate_for_date_range(
-            currency_from, self.base.code, date
-        ).unwrap_or(None)
-        rate_to_base = self._get_rate_for_date_range(
-            self.base.code, currency_to, date
-        ).unwrap_or(None)
-        return rate_from_base, rate_to_base
+            .and_then(Result.as_result(self.rates.append))
+            .is_err()
+        ]
+        return Result.Err(unprocessable) if unprocessable else Result.Ok(None)
 
     def get_rate(
-        self,
-        currency_from: str,
-        currency_to: str,
-        date: str,
+        self, currency_from: str, currency_to: str, date: str
     ) -> Result[Rate, GenericError]:
-        if self._has_base(currency_from, currency_to):
-            return self._get_direct_rate(currency_from, currency_to, date)
-
-        rate_from_base, rate_to_base = self._find_base_rates(
-            currency_from, currency_to, date
-        )
-
-        if rate_from_base is None or rate_to_base is None:
-            return Result.Err(GenericError())
-
-        try:
-            compute_rate = next(rate_from_base.rate.get_atomic_values()) * next(
-                rate_to_base.rate.get_atomic_values()
+        if currency_from == self.base.code:
+            return (
+                next((Some(r) for r in self.rates))
+                .filter(lambda r: r.currency_to.code == currency_to)
+                .ok_or_else(GenericError)
             )
-        except StopIteration:
-            return Result.Err(GenericError())
+        elif currency_to == self.base.code:
+            return (
+                next((Some(r) for r in self.rates))
+                .filter(lambda r: r.currency_to.code == currency_from)
+                .map(lambda r: r.invert_rate().ok())
+                .ok_or_else(GenericError)
+            )
+        else:
+            if (
+                rate_from := self.iter(lambda r: r.currency_to.code == currency_from)
+            ).is_null():
+                return rate_from.ok_or_else(GenericError)
 
-        if (i := Rate.create(currency_from, currency_to, compute_rate, date)).is_err():
-            return Result.Err(GenericError())
-        return Result.Ok(i.unwrap())
+            if (
+                rate_to := self.iter(lambda r: r.currency_to.code == currency_to)
+            ).is_null():
+                return rate_to.ok_or_else(GenericError)
 
-    def list_rates(self) -> Result[Sequence[Rate], GenericError]:
-        if (rates := self.rates_repository.get_all()).is_err():
-            return Result.Err(GenericError())
-        return Result.Ok(rates.unwrap())
+            return rate_from.map(
+                Result.as_result(rate_to.unwrap().multiply)
+            ).map_or_else(lambda: Err(GenericError()), lambda r: r)
+
+    def iter(
+        self,
+        predicate: typing.Callable[[Rate], bool],
+    ) -> Option[Rate, typing.Any]:
+        return next(Some(r) for r in self.rates if predicate(r))
+
+    @Result.as_result
+    def list_rates(self) -> Sequence[Rate]:
+        return tuple(self.rates)
 
     def _has_base(self, currency_from: str, currency_to: str) -> bool:
         return currency_from == self.base.code or currency_to == self.base.code
--- tests/integration_tests/test_agency.py
+++ tests/integration_tests/test_agency.py
@@ -1,11 +1,14 @@
+import typing
 import uuid
+from typing import Sequence
+
 import pytest
 from result import Result
-from typing import Sequence
+
 from currency_convert.domain.agency.entities.agency import Agency, UnprocessedRate
+from currency_convert.domain.agency.valueobjects.currency import Currency
 from currency_convert.domain.agency.valueobjects.money import Money
 from currency_convert.domain.agency.valueobjects.rate import Rate
-from currency_convert.domain.agency.valueobjects.currency import Currency
 from currency_convert.domain.primitives.error import GenericError
 
 
@@ -20,23 +23,30 @@ class FakeRatesRepository:
     def get_all(self) -> Result[Sequence[Rate], GenericError]:
         return Result.Ok(self.rates)
 
+    def get(
+        self, filter: typing.Callable[[Rate], bool]
+    ) -> Result[Sequence[Rate], GenericError]:
+        return self.get_all().map(
+            lambda rates: [rate for rate in rates if filter(rate)]
+        )
+
 
 @pytest.fixture
 def agency() -> Agency:
     repository = FakeRatesRepository()
     return Agency(
         id_=uuid.uuid4(),
-        base=Currency.create("USD").unwrap(),
+        base=Currency.create("USD"),
         name="Test Agency",
         country="Test Country",
-        rates_repository=repository,
+        # rates_repository=repository,
     )
 
 
 def test_add_rate(agency: Agency) -> None:
     result = agency.add_rate("USD", "EUR", "0.85", "2023-10-01")
     assert result.is_ok()
-    assert len(agency.rates_repository.rates) == 1
+    assert len(agency.rates) == 1
 
 
 def test_add_rates(agency: Agency) -> None:
@@ -56,14 +66,14 @@ def test_add_rates(agency: Agency) -> None:
     ]
     result = agency.add_rates(rates)
     assert result.is_ok()
-    assert len(agency.rates_repository.rates) == 2
+    assert len(agency.rates) == 2
 
 
 def test_get_rate(agency: Agency) -> None:
     agency.add_rate("USD", "EUR", "0.85", "2023-10-01")
     result = agency.get_rate("USD", "EUR", "2023-10-01")
     assert result.is_ok()
-    assert result.unwrap().rate == Money.create("0.85").unwrap()
+    assert result.unwrap().rate == Money.create("0.85")
 
 
 def test_list_rates(agency: Agency) -> None:
@@ -92,24 +102,36 @@ def test_add_unprocessable_rates(agency: Agency) -> None:
     result = agency.add_rates(rates)
     assert result.is_err()
     assert len(result.unwrap_err()) == 2
-    assert len(agency.rates_repository.rates) == 0
+    assert len(agency.rates) == 0
+
 
 def test_get_rate_no_base_currency(agency: Agency) -> None:
     # Add rates that do not include the base currency
-    agency.add_rate("EUR", "GBP", "0.85", "2023-10-01")
-    agency.add_rate("GBP", "JPY", "150.00", "2023-10-01")
-    
+    agency.add_rate("USD", "EUR", "0.85", "2023-10-01")
+    agency.add_rate("USD", "JPY", "150.00", "2023-10-01")
+    agency.add_rate("USD", "GBP", "0.75", "2023-10-01")
+
     # Attempt to get a rate that requires the base currency
     result = agency.get_rate("EUR", "JPY", "2023-10-01")
-    assert result.is_err()
+    assert result.is_ok()
+    assert (
+        result.unwrap()
+        == Rate.create(
+            currency_from="EUR",
+            currency_to="JPY",
+            rate="127.5",
+            iso_str="2023-10-01",
+        ).unwrap()
+    )
+
 
 def test_get_rate_with_date_range(agency: Agency) -> None:
     # Add rates for different dates
     agency.add_rate("USD", "EUR", "0.85", "2023-10-01")
     agency.add_rate("USD", "EUR", "0.86", "2023-09-30")
     agency.add_rate("USD", "EUR", "0.87", "2023-09-29")
-    
+
     # Attempt to get a rate for a date with no direct rate, should fallback to previous dates
     result = agency.get_rate("USD", "EUR", "2023-10-02")
     assert result.is_ok()
-    assert result.unwrap().rate == Money.create("0.85").unwrap()
+    assert result.unwrap().rate == Money.create("0.85")
