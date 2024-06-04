b6558f31879f8eb09224310702d7b41591304d20
Markus Iorio <markus.iorio@bfd.de>
2024-06-04 14:55:58 +0200

    exacter cmd and query

--- currency_convert/application/primitives/command.py
+++ currency_convert/application/primitives/command.py
@@ -1,6 +1,16 @@
 import dataclasses
+import typing
+
+from results import Result
+
+T = typing.TypeVar("T")
+E = typing.TypeVar("E")
 
 
 @dataclasses.dataclass(frozen=True)
 class Command:
     pass
+
+
+class CommandHandler(typing.Protocol[T, E]):
+    def execute(self, q: Command) -> Result[T, E]: ...
--- currency_convert/application/primitives/query.py
+++ currency_convert/application/primitives/query.py
@@ -1,6 +1,18 @@
+from __future__ import annotations
 import dataclasses
+import typing
+
+from results import Result
+
+T = typing.TypeVar("T")
+E = typing.TypeVar("E")
+Qcontra = typing.TypeVar("Qcontra", bound="Query", contravariant=True)
 
 
 @dataclasses.dataclass(frozen=True)
 class Query:
     pass
+
+
+class QueryHandler(typing.Protocol[Qcontra, T, E]):
+    def execute(self, query: Qcontra) -> Result[T, E]: ...
